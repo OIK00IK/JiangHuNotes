b2f95aaab611a64fad977ee96528e64c5a64ba09
OIK00IK <watercancer@163.com>
2019-05-07 22:20:51 +0800

    20190507

--- py/application.py
+++ py/application.py
@@ -1,5 +1,3 @@
-import parse
-
 class Application:
     appName = ""
     managementUrl = ""
@@ -17,32 +15,22 @@ class Application:
     usedNonHeapMemory = ""
     nonHeapMemoryPercent = ""
 
-    def dealMemory(self, value):
-        global tempMemory
-        if value > 1000:
-            value = value/1000
-            if value > 1000:
-                value = value/1000
-                tempMemory = str('%.2f' % value) + "G"
-            else:
-                tempMemory = str('%.2f' % value) + "M"
-        return tempMemory
-
     def __init__(self, appName, managementUrl, appId, 
      memory, usedMemory, memoryPercent, 
      heapMemory, usedHeapMemory, heapMemoryPercent, 
      nonHeapMemory, usedNonHeapMemory, nonHeapMemoryPercent
 ):
-        self.name = appName,  
-        self.id = appId,  
-        self.url = managementUrl[7:-5], 
-        self.memory = dealMemory(memory) 
-        self.usedMemory = dealMemory(usedMemory), 
-        self.memoryPercent = str('%.2f' %(memoryPercent * 100)) + "%", 
-        self.heapMemory = dealMemory(heapMemory),  
-        self.usedHeapMemory = dealMemory(usedHeapMemory), 
-        self.heapMemoryPercent = str('%.2f' %(heapMemoryPercent * 100)) + "%", 
-        self.nonHeapMemory = dealMemory(nonHeapMemory), 
-        self.usedNonHeapMemory = dealMemory(usedNonHeapMemory), 
-        self.nonHeapMemoryPercent = str('%.2f' %(nonHeapMemoryPercent * 100)) + "%"
+        self.appName = appName
+        self.managementUrl = managementUrl
+        self.appId = appId
+        self.memory = memory
+        self.usedMemory = usedMemory
+        self.memoryPercent = memoryPercent
+        self.heapMemory = heapMemory
+        self.usedHeapMemory = usedHeapMemory
+        self.heapMemoryPercent = heapMemoryPercent
+        self.nonHeapMemory = nonHeapMemory
+        self.usedNonHeapMemory = usedNonHeapMemory
+        self.nonHeapMemoryPercent = nonHeapMemoryPercent
+        
         
--- py/parse.py
+++ py/parse.py
@@ -1,9 +1,23 @@
 import urllib.request
 import json
+import Application
+
+def dealMemory(value):
+        global tempMemory
+        if value > 1000:
+            value = value/1000
+            if value > 1000:
+                value = value/1000
+                tempMemory = str('%.2f' % value) + "G"
+            else:
+                tempMemory = str('%.2f' % value) + "M"
+        return tempMemory
+
 
 def getIlogList(application,listUrl):       
     appName = application["name"]
     managementUrl = application["managementUrl"]
+    url = managementUrl[7:-5], 
     appId = application["id"]
     response = urllib.request.urlopen(listUrl + appId + "/metrics")
     applicationDetail = response.read().decode("utf-8")
@@ -20,6 +34,12 @@ def getIlogList(application,listUrl):
     nonHeapMemory = json_applicationDetail["nonheap.committed"]
     usedNonHeapMemory = json_applicationDetail["nonheap.used"]
     nonHeapMemoryPercent = usedNonHeapMemory/nonHeapMemory
+
+    app = Application(appName, url, appId, memory, usedMemory, memoryPercent, 
+     heapMemory, usedHeapMemory, heapMemoryPercent, 
+     nonHeapMemory, usedNonHeapMemory, nonHeapMemoryPercent)
+
+    return app
 '''
     ilogApplication = dict(name = appName, id = appId, url = managementUrl[7:-5],
                         memory = dealMemory(memory), usedMemory = dealMemory(usedMemory),
@@ -31,6 +51,7 @@ def getIlogList(application,listUrl):
                         )
     return ilogApplication
 '''
+
 def printIlogList(ilogList): 
     print(len(ilogList))
     for ilog in ilogList:
